be209c2904fd6abe54a2ae8a7a1c773010ca7215
Jon Heller <jon.heller@upstatement.com>
2018-12-18 10:22:44 -0500

    Fixes to block enqueue functions

--- src/Blocks/Blocks.php
+++ src/Blocks/Blocks.php
@@ -13,24 +13,9 @@ class Blocks
      */
     public function __construct()
     {
-        add_action('enqueue_block_assets', array($this,'enqueueBlockAssets'));
         add_action('enqueue_block_editor_assets', array($this,'enqueueBlockEditorAssets'));
 
         new SampleACFBlock\ACFBlock();
-    }
-
-    /**
-     * Enqueue Gutenberg block assets for both frontend + backend.
-     *
-     * @return null
-     */
-    public function enqueueBlockAssets()
-    {
-        wp_enqueue_style(
-            'block-style-css', // Handle.
-            SKELA_THEME_URL . '/dist/block-style.build.css', // Block style CSS.
-            array( 'wp-blocks' ) // Dependency to include the CSS after it.
-        );
     }
 
     /**
@@ -44,7 +29,7 @@ class Blocks
         wp_enqueue_script(
             'block-js', // Handle.
             SKELA_THEME_URL . '/dist/blocks.build.js', // Block.block.build.js: We register the block here. Built with Webpack.
-            array( 'wp-blocks', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
+            array( 'wp-blocks', 'wp-i18n', 'wp-element', 'manifest', 'vendor', 'wp-editor'), // Dependencies, defined above.
             true // Enqueue the script in the footer.
         );
 
