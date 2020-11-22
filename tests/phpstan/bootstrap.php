b8cc82f686e76ed2e7ffcc347acf32c34bef3fee
Viktor Szépe <viktor@szepe.net>
2020-11-22 20:08:26 +0000

    Introduce static analysis

---
+++ tests/phpstan/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define( 'WP_ENV', 'testing' );
+define( 'SKELA_THEME_URL', 'https://' );
+define( 'SKELA_THEME_PATH', './' );
+define( 'SKELA_DOMAIN', 'https://' );
+define( 'SKELA_SITE_NAME', 'skela' );
+define( 'SKELA_THEME_VERSION', '0.0.0' );
